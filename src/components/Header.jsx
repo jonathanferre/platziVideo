25ae8d7bf61ca4d390dc0616551de9c8d6dd5bca
Jonathan <jonathanferre2@gmail.com>
2021-06-01 20:40:31 -0300

    Gravatar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,46 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import { Link } from 'react-router-dom';
+import gravatar from '../utils/gravatar';
 import '../assets/styles/components/Header.scss';
 import logo from '../assets/static/logo-platzi-video-BW2.png';
-import user from '../assets/static/user-icon.png';
+import userIcon from '../assets/static/user-icon.png';
 
-const Header = () => (
+const Header = props => {
+  
+  const { user } = props;
+
+  //Cuntos elementos tiene el objeto user
+  const hasUser = Object.keys(user).length > 0;
+
+  return(
     <header className="header">
-    <Link to='/'>
-      <img className="header__img" src={logo} alt="Platzi Video" />
-    </Link>
-    <div className="header__menu">
-      <div className="header__menu--profile">
-        <img src={user} alt="" />
-        <p>Perfil</p>
+      <Link to='/'>
+        <img className="header__img" src={logo} alt="Platzi Video" />
+      </Link>
+      <div className="header__menu">
+        <div className="header__menu--profile">
+          {hasUser ? 
+            <img src={gravatar(user.email)} alt={user.email} /> :
+            <img src={userIcon} alt="" />
+          }
+          <p>Perfil</p>
+        </div>
+        <ul>
+          <li><a href="/">Cuenta</a></li>
+          <Link to="/login">Iniciar Sesión</Link>
+        </ul>
       </div>
-      <ul>
-        <li><a href="/">Cuenta</a></li>
-        <Link to="/login">Iniciar Sesión</Link>
-      </ul>
-    </div>
-  </header>
-);
+    </header>
+  );
+}
 
-export default Header;
+
+const mapStateToProps = state => {
+  return {
+    user: state.user
+  }
+}
+
+export default connect(mapStateToProps, null)(Header);
